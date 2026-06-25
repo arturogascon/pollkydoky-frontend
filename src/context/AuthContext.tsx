3899a92d4f9faaba06a652ad6209e0c528f6210d
PapArturo <centinela90@gmail.com>
2026-06-25 11:06:16 -0600

    Fixes typescript lint bug

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-  type ReactNode,
-} from "react";
+import { createContext, useContext, useState, type ReactNode } from "react";
 
 interface AuthContextType {
   token: string | null;
